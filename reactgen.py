ddf0adeae9cdd94cd3987f59c67402989ff5939d
Christer Karlsen <chris@ramosicked.com>
2023-02-06 06:28:26 +0000

    upgrade to node19

--- reactgen.py
+++ reactgen.py
@@ -13,18 +13,18 @@ def create_react_docker_container(container_name, public_port):
 
     # Create a new Docker container
     try:
-        subprocess.run(['docker', 'run', '-d', '--name', container_name, '-p', f"{public_port}:3000", 'node:12.18.3-alpine', 'npx', 'create-react-app', container_name])
+        subprocess.run(['docker', 'run', '-d', '--name', container_name, '-p', f"{public_port}:3000", 'node:19', 'npx', 'create-react-app', container_name])
         print(f"Successfully created React Docker container with name: {container_name}")
     except Exception as e:
         print(f"Failed to create React Docker container with name: {container_name}. Error: {e}")
 
     # Create a Dockerfile
     with open(f"{container_name}.Dockerfile", "w") as f:
-        f.write(f"FROM node:12.18.3-alpine\n")
+        f.write(f"FROM node:19\n")
         f.write(f"WORKDIR /app\n")
         f.write(f"COPY . /app\n")
         f.write(f"RUN npm install\n")
-        f.write(f"EXPOSE 3000\n")
+        f.write(f"EXPOSE {public_port}\n")
         f.write(f"CMD [\"npm\", \"start\"]\n")
     print(f"Successfully created Dockerfile for project: {container_name}")
 
@@ -38,3 +38,4 @@ if __name__ == '__main__':
     public_port = args.public_port
 
     create_react_docker_container(container_name, public_port)
+
